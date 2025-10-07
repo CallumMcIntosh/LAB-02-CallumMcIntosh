74d1ea0ad3061102073cad4f4b3245c8fa6631a9
CallumMcI <callum.mcintosh@ontariotechu.net>
2025-10-07 06:39:50 -0400

    full file

---
+++ Assets/PathManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    [HideInInspector]
+    [SerializeField]
+    public List<Waypoint> path;
+
+    public GameObject prefab;
+    int currentPointIndex = 0;
+
+    public List<GameObject> prefabPoints;
+
+    public Waypoint GetNextTarget()
+    {
+        int nextPointIndex = (currentPointIndex + 1) % (path.Count);
+        currentPointIndex = nextPointIndex;
+        return path[nextPointIndex];
+    }
+
+    public List<Waypoint> GetPath()
+    {
+        if (path == null)
+            path = new List<Waypoint>();
+
+        return path;
+    }
+
+    public void CreateAddPoint()
+    {
+        Waypoint go = new Waypoint();
+        path.Add(go);
+    }
+}
